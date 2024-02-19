c6c05fcd64945b4e66bb0830ba4fe798df8b22c9
fatihduzenli <fatihdznl1991@gmail.com>
2024-02-19 16:18:35 -0500

    Baseball/football/soccer team created

---
+++ src/main/java/cydeo/BaseballPlayer.java
@@ -0,0 +1,8 @@
+package cydeo;
+
+public class BaseballPlayer extends Player {
+
+    public BaseballPlayer(String name) {
+        super(name);
+    }
+}
---
+++ src/main/java/cydeo/SoccerPlayer.java
@@ -0,0 +1,7 @@
+package cydeo;
+
+public class SoccerPlayer extends Player{
+    public SoccerPlayer(String name) {
+        super(name);
+    }
+}
