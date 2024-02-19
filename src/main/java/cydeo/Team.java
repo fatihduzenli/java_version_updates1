b6da402d19719c5c000653c163ffd4603c83b253
fatihduzenli <fatihdznl1991@gmail.com>
2024-02-19 17:07:41 -0500

    Generic updated for team class (T extends the Player class)

--- src/main/java/cydeo/Team.java
+++ src/main/java/cydeo/Team.java
@@ -3,15 +3,18 @@ package cydeo;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Team<T> {
+public class Team<T extends Player> { // Should accept only Player class and subclasses
 
     private String name;
 
     private List<T>member=new ArrayList();
 
     public Team(String name) {
+
         this.name = name;
     }
+
+
 
     public String getName() {
         return name;
