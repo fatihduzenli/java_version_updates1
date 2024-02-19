335c7c7046a8a24b2a2446917bf8c2005feee0ac
fatihduzenli <fatihdznl1991@gmail.com>
2024-02-19 17:17:01 -0500

    Main Class updated

--- src/main/java/cydeo/Main.java
+++ src/main/java/cydeo/Main.java
@@ -17,6 +17,21 @@ public class Main {
         printDoubled(items);
 
 
+        System.out.println("**********************************************************");
+
+        FootballPlayer joe = new FootballPlayer("Joe");
+        BaseballPlayer pat =new BaseballPlayer("Pat");
+        SoccerPlayer beckham =new SoccerPlayer("Beckham");
+
+
+        Team <SoccerPlayer>liverpool =new Team("Liverpool");
+       //  liverpool.addPlayer(joe);
+      //  liverpool.addPlayer(pat);
+        liverpool.addPlayer(beckham);
+
+
+
+
 
 
 
