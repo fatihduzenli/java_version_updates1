a0eaabdf585396bc2827a8273a85e38f149d9a57
fatihduzenli <fatihdznl1991@gmail.com>
2024-02-21 00:25:36 -0500

    Apple predicate classes created

---
+++ m3-lambda/src/main/java/com/cydeo/ArraySorting.java
@@ -0,0 +1,23 @@
+package com.cydeo;
+
+public class ArraySorting {
+
+    public static void main(String[] args) {
+
+        ArraySorting as = new ArraySorting();
+
+        QuickSort quickSort = new QuickSort();
+        BubbleSort bubbleSort = new BubbleSort();
+
+
+         as.sort(quickSort);
+         as.sort(bubbleSort);
+
+
+    }
+
+    private void sort(Sorting sorting) {
+
+        sorting.sort();
+    }
+}
